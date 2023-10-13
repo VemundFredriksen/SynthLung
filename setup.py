75704b6e63871d6c0fc6fb39a073e37080b912f1
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2023-10-13 23:56:33 +0200

    Remove commented out code

--- setup.py
+++ setup.py
@@ -6,8 +6,6 @@ setuptools.setup(
     author="Vemund Fredriksen and Svein Ole Matheson Sevle",
     author_email="vemund.fredriksen@hotmailcom",
     description="Package for generating synthetic lung tumors",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
     url="https://github.com/VemundFredriksen/SynthLung",
     packages=setuptools.find_packages(),
     entry_points={"console_scripts": ["synthlung = synthlung.__main__:main"]},
